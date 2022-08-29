e686532dadb7987b26bf31fccd4f1275662b0d88
sigmapng <riabkoyuliia@gmail.com>
2022-08-29 15:12:10 +0300

    Finished mobile footer-nav

--- js/services-tabs.js
+++ js/services-tabs.js
@@ -1,35 +1,30 @@
-(function() {
+(function () {
+  const tabsBtn = document.querySelectorAll(".tabs__nav-btn");
 
-    const tabsBtn = document.querySelectorAll('.tabs__nav-btn');
-    
-    const tabsItem = document.querySelectorAll('.tabs__item');
-    
-    tabsBtn.forEach(onTabClick); 
-    
-    function onTabClick(item) {
-        item.addEventListener("click", function(){
-            let currentBtn = item;
-            let tabId = currentBtn.getAttribute('data-tab');
-            let currentTab = document.querySelector(tabId);
-         
-            if ( ! currentBtn.classList.contains('active') ) {
-         
-            tabsBtn.forEach(function(item) {
-             item.classList.remove('active');
-            });
-         
-            tabsItem.forEach(function(item) {
-             item.classList.remove('active');
-            });
-         
-            currentBtn.classList.add('active');
-            currentTab.classList.add('active');
-         
-            }
+  const tabsItem = document.querySelectorAll(".tabs__item");
+
+  tabsBtn.forEach(onTabClick);
+
+  function onTabClick(item) {
+    item.addEventListener("click", function () {
+      let currentBtn = item;
+      let tabId = currentBtn.getAttribute("data-tab");
+      let currentTab = document.querySelector(tabId);
+
+      if (!currentBtn.classList.contains("active")) {
+        tabsBtn.forEach(function (item) {
+          item.classList.remove("active");
         });
-    }
-    
-    document.querySelector('.tabs__nav-btn').click();
-    
-}) ();
-    
+
+        tabsItem.forEach(function (item) {
+          item.classList.remove("active");
+        });
+
+        currentBtn.classList.add("active");
+        currentTab.classList.add("active");
+      }
+    });
+  }
+
+  document.querySelector(".tabs__nav-btn").click();
+})();
--- js/testimonials-carousel.js
+++ js/testimonials-carousel.js
@@ -1,7 +1,6 @@
 (function () {
-
-    const slides = [
-        `<div class ="testimonials__slide">
+  const slides = [
+    `<div class ="testimonials__slide">
             <img src="img/testimonials/client-01.png" alt="Client 01">
             <h3 class="name__client">Josh Mendel</h3>
             <p class="testimonials__text">“I really love to drink coffee in the morning, but I hate making it.
@@ -10,7 +9,7 @@
             done quickly, they brought and set up the coffee machine.”</p>
           <p>December 15, 2020</p>
         </div>`,
-        `<div class ="testimonials__slide">
+    `<div class ="testimonials__slide">
             <img src="img/testimonials/client-02.png" alt="Client 02">
             <h3 class="name__client">Jessica Malacks</h3>
             <p class="testimonials__text">“We have renovated the office kitchen and decided to delight our employees
@@ -19,7 +18,7 @@
               machines from this company and were completely satisfied.”</p>
             <p>February 10, 2021</p>
         </div>`,
-        `<div class ="testimonials__slide">
+    `<div class ="testimonials__slide">
             <img src="img/testimonials/client-03.png" alt="Client 03">
             <h3 class="name__client">Daniella Williamson</h3>
             <p class="testimonials__text">“When I opened a coffee shop, I couldn't find an affordable coffee machine.
@@ -28,7 +27,7 @@
               the coffee machine and regularly deliver coffee beans and cups." </p>
             <p>March 04, 2021</p>
         </div>`,
-        `<div class ="testimonials__slide">
+    `<div class ="testimonials__slide">
             <img src="img/testimonials/client-04.png" alt="Client 04">
             <h3 class="name__client">Dean Steinberg</h3>
             <p class="testimonials__text">“We have been cooperating with this company for 2 years already, and during
@@ -36,44 +35,49 @@
             single problem situation has arisen. The service is fast, the coffee is delivered without
             delay, the prices for coffee machine rental are very low.”</p>
             <p>March 22, 2021</p>
-        </div>`
-    ];
-    
-    let currentSlideIdx  = 0;
+        </div>`,
+  ];
 
-    function renderCarousel() {
-        const slideContainer = document.querySelector('.testimonials-carousel__slides');
-        slideContainer.innerHTML = slides[currentSlideIdx];
-        if (window.innerWidth > 600) {
-            const secondSlideIdx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
-            slideContainer.innerHTML += slides[secondSlideIdx];
-            if (window.innerWidth > 900) {
-                const thirdSlideIdx = secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
-                slideContainer.innerHTML += slides[thirdSlideIdx];
-            }
-        }
+  let currentSlideIdx = 0;
+
+  function renderCarousel() {
+    const slideContainer = document.querySelector(
+      ".testimonials-carousel__slides"
+    );
+    slideContainer.innerHTML = slides[currentSlideIdx];
+    if (window.innerWidth > 600) {
+      const secondSlideIdx =
+        currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
+      slideContainer.innerHTML += slides[secondSlideIdx];
+      if (window.innerWidth > 900) {
+        const thirdSlideIdx =
+          secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
+        slideContainer.innerHTML += slides[thirdSlideIdx];
+      }
     }
+  }
 
-    function prev() {
-        currentSlideIdx = currentSlideIdx - 1 < 0 ? slides.length - 1 : currentSlideIdx - 1;
-        renderCarousel();
-    }
-
-    function next() {
-        currentSlideIdx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
-        renderCarousel();
-    }
-
-    //setInterval(next, 3000);
-
-    const prevButton = document.querySelector('.testimonials-carousel__btn-prev');
-    prevButton.addEventListener('click', prev);
-
-    const nextButton = document.querySelector('.testimonials-carousel__btn-next');
-    nextButton.addEventListener('click', next);
-
+  function prev() {
+    currentSlideIdx =
+      currentSlideIdx - 1 < 0 ? slides.length - 1 : currentSlideIdx - 1;
     renderCarousel();
+  }
 
-    window.addEventListener('resize', renderCarousel);
+  function next() {
+    currentSlideIdx =
+      currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
+    renderCarousel();
+  }
 
+  //setInterval(next, 3000);
+
+  const prevButton = document.querySelector(".testimonials-carousel__btn-prev");
+  prevButton.addEventListener("click", prev);
+
+  const nextButton = document.querySelector(".testimonials-carousel__btn-next");
+  nextButton.addEventListener("click", next);
+
+  renderCarousel();
+
+  window.addEventListener("resize", renderCarousel);
 })();
